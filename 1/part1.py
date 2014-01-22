fa75ee8c7de50ef1350b9646cbb5432a72388424
JohnaLiu <zhijiaoliu@dhcp-10-201-109-68.tamulink.tamu.edu>
2014-01-22 10:30:17 -0600

    IR 2014 Spring

---
+++ 1/part1.py
@@ -0,0 +1,10 @@
+"""
+Description: Implement the core indexing and retrieval features of a boolean search engine in Python
+Requirement: HW1: http://courses.cse.tamu.edu/caverlee/csce670/hw/hw1.html
+File name: part1.py
+Programmer: Zhijiao Liu (Johna)
+Last modified date: 
+"""
+#
+#
+#
