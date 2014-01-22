42860f8c5f68285e1ebec50bbe90659a73f419c1
JohnaLiu <zhijiaoliu@dhcp-10-201-109-68.tamulink.tamu.edu>
2014-01-22 11:36:04 -0600

    change dir

--- 1/part1.py
+++ 1/part1.py
@@ -3,8 +3,34 @@ Description: Implement the core indexing and retrieval features of a boolean sea
 Requirement: HW1: http://courses.cse.tamu.edu/caverlee/csce670/hw/hw1.html
 File name: part1.py
 Programmer: Zhijiao Liu (Johna)
+Environment: 
 Last modified date: 
 """
-#
-#
-#
+
+import os
+
+
+def getDirList(currentDir):
+	currentFile = os.listdir(currentDir)
+#	print os.listdir(currentDir)
+	for f in currentFile:
+		if os.path.isdir(currentDir+"/"+f):
+			return currentDir+"/"+f
+		elif os.path.isfile(currentDir+"/"+f):
+			return currentDir
+
+
+currentDir = os.getcwd()
+#currentFile = os.listdir(currentDir)
+for f in os.listdir(currentDir):
+	if os.path.isdir(currentDir+"/"+f):
+		os.listdir(currentDir)
+		currentDir = getDirList(currentDir)
+
+		print currentDir
+	elif os.path.isfile(currentDir):
+		pass
+
+#print os.chdir(getDirList(currentDir))
+
+#print getDirList(currentDir)
